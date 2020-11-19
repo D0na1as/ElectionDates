e58d7442c5ea37e3e8af4483fa7f8c89caf8aea4
D0na1as <d.kadakauskis@gmail.com>
2020-11-19 14:24:40 +0200

    Initial commit

---
+++ src/main/java/com/api/dates/ElectionDatesApplication.java
@@ -0,0 +1,13 @@
+package com.api.dates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionDatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectionDatesApplication.class, args);
+	}
+
+}
