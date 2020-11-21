993c557aabe3476c55ce26ac13f2d16c81938815
D0na1as <d.kadakauskis@gmail.com>
2020-11-21 21:42:05 +0200

    Add heroku.yml

--- src/main/java/com/api/dates/ElectionDatesApplication.java
+++ src/main/java/com/api/dates/ElectionDatesApplication.java
@@ -2,12 +2,34 @@ package com.api.dates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ElectionDatesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElectionDatesApplication.class, args);
+	}
+
+	@Bean
+	public TomcatServletWebServerFactory tomcatCustomizer() {
+		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+		factory.addConnectorCustomizers((connector -> {
+
+			/**
+			 * Gets PORT env variable and sets it as tomcat server port.
+			 * In Heroku env, the PORT env variable must be used by tomcat
+			 * because if not the process will fail.
+			 * Error R10 (Boot timeout) -> Web process failed to bind to $PORT within 60 seconds of launch
+			 */
+			String envPort = System.getenv("PORT");
+			// If PORT is NULL, we'll use the default port
+			if (envPort != null) {
+				connector.setPort(Integer.parseInt(envPort));
+			}
+		}));
+		return factory;
 	}
 
 }
